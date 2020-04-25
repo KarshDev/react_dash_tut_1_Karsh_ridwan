57147d14dfefad58e51fda48502d8dd93c177348
Karsh <karshie04@gmail.com>
2020-04-25 19:14:57 +0100

    build

---
+++ build/precache-manifest.30ffd4a222b04338c97167f7161764e1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02eb06ce5033fe4c427d0286cf27302c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4dca89e53f18b62aaef9",
+    "url": "/static/css/2.25b83785.chunk.css"
+  },
+  {
+    "revision": "270433d812c9251c5364",
+    "url": "/static/css/main.13b1f3b5.chunk.css"
+  },
+  {
+    "revision": "4dca89e53f18b62aaef9",
+    "url": "/static/js/2.56eef0b3.chunk.js"
+  },
+  {
+    "revision": "a13369e7405157f254f1ffcc4cc651e5",
+    "url": "/static/js/2.56eef0b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "270433d812c9251c5364",
+    "url": "/static/js/main.7c42ec85.chunk.js"
+  },
+  {
+    "revision": "2b8485a4abb8e55a0676",
+    "url": "/static/js/runtime-main.926ed7d0.js"
+  },
+  {
+    "revision": "13ef868eb1c884f387758f8283d61e8c",
+    "url": "/static/media/profile.13ef868e.jpg"
+  },
+  {
+    "revision": "152d4c0255709b49bf6f9db4ff56e781",
+    "url": "/static/media/solab-logo2.152d4c02.png"
+  }
+]);
